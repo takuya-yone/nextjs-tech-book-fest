e577a4ab149499612196bd03de60d740b8278a21
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-10-23 05:50:34 +0900

    wip

--- src/app/utils/sendDataToRum.ts
+++ src/app/utils/sendDataToRum.ts
@@ -5,15 +5,15 @@ const sendDataToRum = (): void => {
     const config: AwsRumConfig = {
       sessionSampleRate: 1,
       guestRoleArn:
-        "arn:aws:iam::123456789012:role/RUM-Monitor-ap-northeast-1-123456789012-5880751008961-Unauth",
-      identityPoolId: "ap-northeast-1:4c9b40d2-6a82-4b14-8e23-aaaaaaaaaa",
+        "arn:aws:iam::012345689012:role/RUM-Monitor-ap-northeast-1-012345689012-111111111111-Unauth",
+      identityPoolId: "ap-northeast-1:4c9b40d2-6a82-4b14-8e23-xxxxxxxx",
       endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com",
       telemetries: ["performance", "errors", "http"],
       allowCookies: true,
       enableXRay: true,
     };
 
-    const APPLICATION_ID: string = "287874cb-9a11-48cc-b6cf-aaaaaaaaaaaa";
+    const APPLICATION_ID: string = "287874cb-9a11-48cc-b6cf-xxxxxxxxxx";
     const APPLICATION_VERSION: string = "1.0.0";
     const APPLICATION_REGION: string = "ap-northeast-1";
 
