b7a7eb488cdc5b8fd6563c2b3ce6913cfd3cd051
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-10-29 18:30:38 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    trailingSlash: true
+    trailingSlash: true,
+    images: { unoptimized: true } //追記
 }
 
 module.exports = nextConfig
